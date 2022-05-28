bf748a60c88a0f6c5d4a4b995e6da581eb8442c1
VictorMPA96 <vicmapeal96@gmail.com>
2022-05-28 04:31:42 +0100

    back done

--- controllers/users/controllerPOST.js
+++ controllers/users/controllerPOST.js
@@ -45,7 +45,7 @@ exports.loginUser = async (req, res) => {
             const login = {
                 sub: moment().unix(),
                 idUser: user.id,
-                iat: moment().add(10, "minutes").unix()
+                iat: moment().add(5, "minutes").unix()
             }
             
             const token = jwt.sign(login, 'secretkey');   
